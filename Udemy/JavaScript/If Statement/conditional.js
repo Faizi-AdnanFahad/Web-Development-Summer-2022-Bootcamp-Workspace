dd28e6a6e7fb1e1a891e55e5dbc54034aa0a1d2c
Adnan Fahad Faizi <adnan500@my.yorku.ca>
2022-06-09 11:24:07 -0400

    if statement

---
+++ Udemy/JavaScript/If Statement/conditional.js
@@ -0,0 +1,15 @@
+console.log("BEFORE CONDITIONAL")
+if (1 + 1 === 2) {
+    console.log("1 + 1 is equal to 2")
+}
+console.log("AFTER CONDITIONAL")
+
+console.log("--------------------------")
+
+let random = Math.random();
+if (random < 0.5) {
+    console.log("Your number is less than 0.5: " + random)
+}
+else {
+    console.log("Your number is greater than 0.5: " + random)
+}
