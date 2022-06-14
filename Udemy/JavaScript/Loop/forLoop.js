86446a3c9b2f620ef463b2ad8711466a675ad83c
Adnan Fahad Faizi <adnan500@my.yorku.ca>
2022-06-14 17:42:00 -0400

    For & While loops

---
+++ Udemy/JavaScript/Loop/forLoop.js
@@ -0,0 +1,16 @@
+for (let i = 0; i < 10; i ++) {
+    console.log(i);
+}
+
+console.log("-------------------------------------------")
+
+// Odd numbers
+for (let i = 1; i < 21; i += 2) {
+    console.log(i);
+}
+
+console.log("-------------------------------------------")
+
+for (let i = 100; i > -1; i -= 10) {
+    console.log(i);
+}
---
+++ Udemy/JavaScript/Objects/addingObjects.js
@@ -0,0 +1,29 @@
+const midterm = {
+    daniella: 96,
+    tom: 60
+}
+
+midterm.tom = 90;
+midterm.john = 89;
+
+console.log(midterm);
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+// Objects of arrays
+
+const comment = [
+    {
+        username: 'Tammy',
+        text: 'lolololo', 
+        votes: 9,
+    },
+    {
+        username: 'John',
+        text: 'glub glub', 
+        votes: 10,
+    }
+
+]
+
+console.log(comment[1].text); // glub glub
