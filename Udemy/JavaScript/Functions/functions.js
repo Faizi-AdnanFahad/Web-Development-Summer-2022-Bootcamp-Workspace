8cab74a72660e9c001c31e3e44482624b9b14c82
Adnan Fahad Faizi <adnan500@my.yorku.ca>
2022-06-15 16:39:16 -0400

    More functions

---
+++ Udemy/JavaScript/Functions/functions.js
@@ -0,0 +1,31 @@
+// // ***********Basic Functions**********
+// // Our first Function
+
+// function singSong() {
+//     console.log("WOOOOHHHHHH");
+//     console.log("Do");
+//     console.log("Wowowowowowo");
+//     console.log("Hey Hey Hey");
+// }
+
+// // singSong();
+
+// function greet(person) {
+//     console.log(`Hi, ${person}`);
+// }
+
+// function greet(person1, person2) {
+//     console.log(`${person1} LOVES ${person2}`);
+// }
+
+function repeat(str, n) {
+    let result = "";
+    for (let i = 0; i < n; i ++) {
+        result += str;
+    }
+    console.log(result);
+}
+
+repeat("$ ", 4);
+
+// ***********Intermediate Functions**********
