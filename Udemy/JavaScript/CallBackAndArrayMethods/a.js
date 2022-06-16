f6fb195010c50125d575b6b8395eb1795ddf7914
Adnan Fahad Faizi <adnan500@my.yorku.ca>
2022-06-16 19:44:59 -0400

    setTimeOut & setInterval

---
+++ Udemy/JavaScript/CallBackAndArrayMethods/a.js
@@ -0,0 +1,31 @@
+// const numbers = [5, 10, 15, 20];
+
+// numbers.forEach(function(i) {
+//     console.log(i/5);
+// })
+
+// /*******************************/
+// const nums = [1, 2, 3, 4];
+// const doubles = nums.map(function(i) {
+//     return i * 2;
+// }) // doubles = [2, 4, 6, 8]
+// /*******************************/
+
+// // Arrow Function
+// let rollDie = (num) => {
+//     return console.log(Math.floor(Math.random() * 6) + 1);
+// }
+// /*******************************/
+
+/*Prints after 3 seconds*/
+setTimeout(() => {
+    console.log("Hi");
+}, 3000);
+
+/*Prints every 1 second*/
+let id = setInterval(() => {
+    console.log("Hi");
+}, 1000);
+
+// clearInterval(id); stops the iterval
+// /*******************************/
