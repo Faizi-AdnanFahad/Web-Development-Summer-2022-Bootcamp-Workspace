dcea369ecd524f5bd6407fe30becc81ee10c63a9
Adnan Fahad Faizi <adnan500@my.yorku.ca>
2022-06-16 20:48:32 -0400

    Spread on Objects

--- Udemy/JavaScript/NewerJavascript/newer.js
+++ Udemy/JavaScript/NewerJavascript/newer.js
@@ -14,3 +14,9 @@ const oddsCopy = [...odds]; // [1, 3, 5, 7]
 let evens = [2, 4, 6, 8];
 const evenCopy = [...evens, ...odds]; // [2, 4, 6, 8, 1, 3, 5, 7]
 
+// Spread with 'Objects'
+let obj1 = {name: "ali", age: "12"};
+let obj2 = {fName: "Rambo", age: "100"};
+let objMixed = {...obj1, ...obj2}; //{name: 'ali', age: '100', fName: 'Rambo'}
+let objExtra = {...obj1, city: "Kabul"} // {name: 'ali', age: '12', city: 'Kabul'}
+let objArrWithIndex = {..."HELLO"}; // {0: 'H', 1: 'E', 2: 'L', 3: 'L', 4: 'O'}
