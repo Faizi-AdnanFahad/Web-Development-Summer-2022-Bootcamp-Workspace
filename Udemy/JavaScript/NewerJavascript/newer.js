ad95032742255dcff79744dadb82438a34461570
Adnan Fahad Faizi <adnan500@my.yorku.ca>
2022-06-16 21:03:53 -0400

    ...REST
    Collapsing all arguments inside 1 array

--- Udemy/JavaScript/NewerJavascript/newer.js
+++ Udemy/JavaScript/NewerJavascript/newer.js
@@ -2,7 +2,7 @@
 function rollDie(num = 6) {
     return Math.floor(Math.random() * num) + 1;
 }
-
+/*************************************************************/
 // Spread: Spreading each elements of an array as an argument
 console.log(Math.max(...[1, 5, 23, 6,23, 132,5, 54])); 
 /*Same as*/
@@ -20,3 +20,18 @@ let obj2 = {fName: "Rambo", age: "100"};
 let objMixed = {...obj1, ...obj2}; //{name: 'ali', age: '100', fName: 'Rambo'}
 let objExtra = {...obj1, city: "Kabul"} // {name: 'ali', age: '12', city: 'Kabul'}
 let objArrWithIndex = {..."HELLO"}; // {0: 'H', 1: 'E', 2: 'L', 3: 'L', 4: 'O'}
+
+/*************************************************************/
+// Rest
+function restDemo(...arg1) { // The 3 dotes will put all arguments in an array
+    console.log(arg1);
+    console.log(arg1[0]);
+}
+restDemo("Tina", "John") // ['Tina', 'John']
+// We can also use Rest to have normal arguments and unlimited array of arguments
+function restDemo2(person1, person2, ...arg1) {
+    console.log(`GOLD: ${person1}, SILVER: ${person2}, The rest of the participants are ${arg1}`);
+}
+restDemo2("Tina", "John", 'Ali', 'Karim'); // GOLD: Tina, SILVER: John, The rest of the participants are Ali,Karim
+
+/*************************************************************/
