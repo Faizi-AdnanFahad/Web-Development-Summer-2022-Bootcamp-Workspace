2e5100cdd8d33811c300938798932d3c1230980a
Adnan Fahad Faizi <adnan500@my.yorku.ca>
2022-06-21 17:05:10 -0400

    Reset Button Done

--- Udemy/DOM/DOM Events/The Ping Pong Game/pingPong.js
+++ Udemy/DOM/DOM Events/The Ping Pong Game/pingPong.js
@@ -6,6 +6,14 @@ let player2BTN = document.querySelector('#player2BTN');
 let secondPlayerScore = document.querySelector('#secondPlayerScore');
 let value2 = parseInt(secondPlayerScore.innerText);
 
+let resetBTN = document.querySelector('#resetBTN');
+
+function incrementScore(player, value) {
+    value ++;
+    player.innerText = value;
+    return value;
+}
+
 player1BTN.addEventListener('click', function() {
     value1 = incrementScore(firstPlayerScore, value1);
 
@@ -15,9 +23,10 @@ player2BTN.addEventListener('click', function() {
     value2 = incrementScore(secondPlayerScore, value2);
 });
 
-function incrementScore(player, value) {
-    value ++;
-    console.log(value);
-    player.innerText = value;
-    return value;
-}
+resetBTN.addEventListener('click', function() {
+    /* Reset Everything */
+    firstPlayerScore.innerText = 0;
+    secondPlayerScore.innerText = 0;
+    value1 = 0;
+    value2 = 0;
+})
