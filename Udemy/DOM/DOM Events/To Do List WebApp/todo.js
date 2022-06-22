2eb31970a5209911a6ac8e267177a9d2962f090a
Adnan Fahad Faizi <adnan500@my.yorku.ca>
2022-06-22 16:30:35 -0400

    overflow added

--- Udemy/DOM/DOM Events/To Do List WebApp/todo.js
+++ Udemy/DOM/DOM Events/To Do List WebApp/todo.js
@@ -3,10 +3,13 @@ let addBTN = document.querySelector("#add");
 let container = document.querySelector("#container");
 
 
-addBTN.addEventListener('click', function() {
+addBTN.addEventListener('click', function () {
     if (todoInput.value) {
         let div = document.createElement('div');
         div.classList.add("newToDoDiv");
+        if (todoInput.value.length >= 88) {
+            newDiv.style = 'overflow: scroll';
+        }
         div.innerText = todoInput.value;
         container.appendChild(div);
         todoInput.value = '';
@@ -16,16 +19,18 @@ addBTN.addEventListener('click', function() {
     }
 });
 
-todoInput.addEventListener('keydown', function(event) {
-    
+todoInput.addEventListener('keydown', function (event) {
     if (todoInput.value && event.key === 'Enter') {
-        let div = document.createElement('div');
-        div.classList.add("newToDoDiv");
-        div.innerText = todoInput.value;
-        container.appendChild(div);
+        let newDiv = document.createElement('div');
+        if (todoInput.value.length >= 88) {
+            newDiv.style = 'overflow: scroll';
+        }
+        newDiv.classList.add("newToDoDiv");
+        newDiv.innerText = todoInput.value;
+        container.appendChild(newDiv);
         todoInput.value = '';
     }
     else if (event.key === 'Enter' && todoInput.value === '') {
         alert("Can not add empty todo task!")
-    } 
+    }
 });
