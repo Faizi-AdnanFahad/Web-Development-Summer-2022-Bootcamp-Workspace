c35004a45bf26a14e903f4330d273ecd003ff20b
Adnan Fahad Faizi <adnan500@my.yorku.ca>
2022-06-22 16:08:16 -0400

    task addition by 'Entering'

--- Udemy/DOM/DOM Events/To Do List WebApp/todo.js
+++ Udemy/DOM/DOM Events/To Do List WebApp/todo.js
@@ -2,11 +2,30 @@ let todoInput = document.querySelector('#todoInput');
 let addBTN = document.querySelector("#add");
 let container = document.querySelector("#container");
 
+
 addBTN.addEventListener('click', function() {
-    console.log(todoInput.value);
-    let div = document.createElement('div');
-    div.classList.add("newToDoDiv");
-    div.innerText = todoInput.value;
-    container.appendChild(div);
-    todoInput.value = '';
+    if (todoInput.value) {
+        let div = document.createElement('div');
+        div.classList.add("newToDoDiv");
+        div.innerText = todoInput.value;
+        container.appendChild(div);
+        todoInput.value = '';
+    }
+    else {
+        alert("Can not add empty todo task!")
+    }
+});
+
+todoInput.addEventListener('keydown', function(event) {
+    
+    if (todoInput.value && event.key === 'Enter') {
+        let div = document.createElement('div');
+        div.classList.add("newToDoDiv");
+        div.innerText = todoInput.value;
+        container.appendChild(div);
+        todoInput.value = '';
+    }
+    else if (event.key === 'Enter' && todoInput.value === '') {
+        alert("Can not add empty todo task!")
+    } 
 });
