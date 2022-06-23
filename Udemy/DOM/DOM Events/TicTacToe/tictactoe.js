2b24bf8600dd444fa0141d6e7c9091a8232cbef1
Adnan Fahad Faizi <adnan500@my.yorku.ca>
2022-06-23 16:41:08 -0400

    players hover click complete

--- Udemy/DOM/DOM Events/TicTacToe/tictactoe.js
+++ Udemy/DOM/DOM Events/TicTacToe/tictactoe.js
@@ -14,21 +14,32 @@ for (let i = 0; i < 3; i++) {
 
 for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
-
-
+        var playerTurn = 'X';
         ['mouseenter', 'mouseleave', 'click'].forEach(function (evt) {
             cells[i][j].addEventListener(evt, function () {
-                if (cells[i][j].innerText === 'XXXXX') {
+                if (cells[i][j].innerText === 'XXXXX' || cells[i][j].innerText === 'OOOOO') {
                     cells[i][j].style.cursor = 'not-allowed';
                 }
                 else {
                     if (evt === 'click') {
-                        cells[i][j].innerText = 'XXXXX';
+                        if (playerTurn === 'X') {
+                            cells[i][j].innerText = 'XXXXX';
+                            playerTurn = 'O';
+                        }
+                        else {
+                            cells[i][j].innerText = 'OOOOO';
+                            playerTurn = 'X';
+                        }
                     }
-                    else if (cells[i][j].innerText !== 'XXXXX' && evt === 'mouseenter') {
-                        cells[i][j].innerText = 'X';
+                    else if (!(cells[i][j].innerText === 'XXXXX' || cells[i][j].innerText === 'OOOOO') && evt === 'mouseenter') {
+                        if (playerTurn === 'X') {
+                            cells[i][j].innerText = 'X';
+                        }
+                        else {
+                            cells[i][j].innerText = 'O';
+                        }
                     }
-                    else if (cells[i][j].innerText !== 'XXXXX' && evt === 'mouseleave') {
+                    else if (!(cells[i][j].innerText === 'XXXXX' || cells[i][j].innerText === 'OOOOO')  && evt === 'mouseleave') {
                         cells[i][j].innerText = '';
                     }
                 }
