3ecbf832d4e41a66abd0815775c39d9e58721792
Adnan Fahad Faizi <adnan500@my.yorku.ca>
2022-06-23 15:17:43 -0400

    click, mouseEnter, mouseLeave implemented on player 'X'

--- Udemy/DOM/DOM Events/TicTacToe/tictactoe.js
+++ Udemy/DOM/DOM Events/TicTacToe/tictactoe.js
@@ -1,22 +1,33 @@
 let cells = [
-    [], 
+    [],
     [],
     []
 ];
 
 /* Initalizing the 2-d array to the list of divs */
-for (let i = 0; i < 3; i ++) {
+for (let i = 0; i < 3; i++) {
     let currentRow = document.querySelector(`#row:nth-of-type(${i + 1})`);
-    for (let j = 0; j < 3; j ++) {
+    for (let j = 0; j < 3; j++) {
         cells[i][j] = currentRow.children[j];
     }
 }
 
-for (let i = 0; i < 3; i ++) {
-    for (let j = 0; j < 3; j ++) {
-        cells[i][j].addEventListener('mouseup', function() {
-            // console.dir(cells[i][j]);
-            cells[i][j].innerText = 'X';
-        })
+for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+
+
+        ['mouseenter', 'mouseleave', 'click'].forEach(function (evt) {
+            cells[i][j].addEventListener(evt, function () {
+                if (evt === 'click') {
+                    cells[i][j].innerText = 'XXXXX';
+                }
+                else if (cells[i][j].innerText !== 'XXXXX' && evt === 'mouseenter') {
+                    cells[i][j].innerText = 'X';
+                }
+                else if (cells[i][j].innerText !== 'XXXXX' && evt === 'mouseleave') {
+                    cells[i][j].innerText = '';
+                }
+            }, false);
+        });
     }
 }
