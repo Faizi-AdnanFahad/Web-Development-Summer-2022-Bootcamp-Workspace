47632706519595cb0c681051499cdc6dde1df5e8
Adnan Fahad Faizi <adnan500@my.yorku.ca>
2022-06-28 15:06:04 -0400

    search customized based on user inputs

--- Udemy/Projects/Small Projects/Tv Show Searcher/app.js
+++ Udemy/Projects/Small Projects/Tv Show Searcher/app.js
@@ -1,9 +1,21 @@
 let searchForm = document.querySelector('#search');
 
-searchForm.addEventListener('click', function(event) {
+
+
+async function searchForQuery(inputForm) {
+    let config = {
+        params: {
+            q: inputForm.value,
+        }
+    }
+    let data = await axios.get('https://api.tvmaze.com/search/shows?q=', config);
+    console.log(data.data);
+}
+
+searchForm.addEventListener('submit', function(event) {
     event.preventDefault();
     let input = searchForm.elements[0];
-    
+    searchForQuery(input);
 
     input.value = ''; // Clean the user entered search
 })
