09c9effce3e86d2627b63fcea022e2f668ed1767
Adnan Fahad Faizi <adnan500@my.yorku.ca>
2022-06-28 15:21:19 -0400

    Div element for holding info created

--- Udemy/Projects/Small Projects/Tv Show Searcher/app.js
+++ Udemy/Projects/Small Projects/Tv Show Searcher/app.js
@@ -1,5 +1,15 @@
 let searchForm = document.querySelector('#search');
 
+searchForm.addEventListener('submit', async function(event) {
+    event.preventDefault();
+    let input = searchForm.elements[0];
+    // let data = await searchForQuery(input);
+    // console.log(data.data);
+
+    createDivMovie();
+
+    input.value = ''; // Clean the user entered search
+})
 
 
 async function searchForQuery(inputForm) {
@@ -8,14 +18,16 @@ async function searchForQuery(inputForm) {
             q: inputForm.value,
         }
     }
-    let data = await axios.get('https://api.tvmaze.com/search/shows?q=', config);
-    console.log(data.data);
+    return await axios.get('https://api.tvmaze.com/search/shows?q=', config);
 }
 
-searchForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-    let input = searchForm.elements[0];
-    searchForQuery(input);
+function createDivMovie() {
+    let newDiv = document.createElement('DIV');
+    newDiv.id = 'divMovieInfo';
 
-    input.value = ''; // Clean the user entered search
-})
+    let newIMG = document.createElement('IMG');
+    newIMG.innerText = 'new IMAGE';
+
+    newDiv.appendChild(newIMG);
+    document.body.appendChild(newDiv);
+}
